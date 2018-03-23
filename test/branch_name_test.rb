e1116327f60e731e7304db3744bbb708d61db29a
Jens Balvig <jens@balvig.com>
2018-03-23 19:00:39 +0100

    fix spec

--- test/branch_name_test.rb
+++ test/branch_name_test.rb
@@ -2,11 +2,16 @@ require "test_helper"
 
 module Cp8Cli
   class BranchNameTest < Minitest::Test
+    def setup
+      stub_shell
+    end
+
     def test_branch_name_for_story
+      stub_github_user("Doug Adams")
       user = CurrentUser.new
       name = BranchName.new(user: user, branch_identifier: "cookpad/cp-8#1234").to_s
 
-      assert_equal "jb/cookpad/cp-8#1234", name
+      assert_equal "da/cookpad/cp-8#1234", name
     end
   end
 end
